198c6a6a1d43626c1f8730fd8b03f4a7bfe22160
Kate <knaychuk@gmail.com>
2023-09-06 21:54:12 -0700

    clerkinstance error fixed

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
-
 import "../globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,11 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-    
-    >
+    <ClerkProvider>
       <html lang='en'>
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+        <body className={`${inter.className} bg-slate-900`}>{children}</body>
       </html>
     </ClerkProvider>
   );
