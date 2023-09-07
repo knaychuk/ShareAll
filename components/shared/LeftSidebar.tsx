c4d0eee2479f3e56b26a8758a6f9a233d80caf1b
Kate <knaychuk@gmail.com>
2023-09-07 11:55:43 -0700

    leftsidebar icons + labels

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,6 +1,28 @@
+import { sidebarLinks } from "@/constants"
+import Image from "next/image"
+import Link from "next/link"
+
 const LeftSidebar = () => {
   return (
-    <div>LeftSidebar</div>
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
+        {sidebarLinks.map((link) => (
+          <Link
+            key={link.label} 
+            href={link.route}
+            className="leftsidebar_link"
+          >
+            <Image 
+              src={link.imgURL}
+              alt={link.label}
+              width={24}
+              height={24}
+            />
+            <p className="text-light-1 max-lg:hidden">{link.label}</p>
+          </Link>
+        ))}
+      </div>
+    </section>
   )
 }
 export default LeftSidebar
